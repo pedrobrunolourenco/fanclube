3903c4f97646ac5f3afa880c29b784149b3172e5
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-06-06 21:00:44 -0300

    02-Adicionado React-Router-Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes";
+
 
 export const App = () => {
   return (
-    <div className="App">
-         TESTE
-    </div>
+    <BrowserRouter>
+       <AppRoutes />
+    </BrowserRouter>
   );
 }
 
