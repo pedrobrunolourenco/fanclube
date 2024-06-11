a7722e4095a897934d3cfdb3d953be83e73a8419
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-06-11 15:37:11 -0300

    022 - Commit

--- src/pages/admiradores/DetalheDeAdmiradores.tsx
+++ src/pages/admiradores/DetalheDeAdmiradores.tsx
@@ -2,77 +2,190 @@ import { useNavigate, useParams } from "react-router-dom";
 import { LayOutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components";
 import { useEffect, useState } from "react";
-import { AdmiradoresService } from "../../shared/services/api/admiradores/AdmiradoresService";
-import { LinearProgress } from "@mui/material";
+import { AdmiradoresService, IDetalheAdmirador } from "../../shared/services/api/admiradores/AdmiradoresService";
 
+import { TextField, Box, Stack } from "@mui/material";
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
 
+type FormValues = {
+   id: number;
+   nomeCompleto: string;
+   email: string;
+   idade: number;
+   notavelId: number;
+}
 
 export const DetalheDeAdmiradores: React.FC = () => {
-
-    const { id = 'novo'}  = useParams<'id'>();
+    const { id = 'novo' } = useParams<{ id: string }>();
     const navigate = useNavigate();
-
-    const [isLoading, setIsLoading] = useState(false);
     const [nome, setNome] = useState("");
 
-    
+    const { register, handleSubmit, formState, control, setValue } = useForm<FormValues>({
+        defaultValues: {
+            id: id === 'novo' ? undefined : Number(id),
+            nomeCompleto: '',
+            email: '',
+            idade: 0,
+            notavelId: 0,
+        }
+    });
+    const { errors } = formState;
+
+    const onSubmit = (data: FormValues) => {
+        handleSalvar(data);
+    };
+
     const handleDelete = (id: number) => {
         AdmiradoresService.deleteById(id)
         .then(result => {
             if (result instanceof Error) {
-              alert(result.message);
+                alert(result.message);
             } else {
-               alert('Registro apagado com sucesso!');
-               navigate('/admiradores');
+                alert('Registro apagado com sucesso!');
+                navigate('/admiradores');
             }
         });
     }
 
-    useEffect( () => {
-        if(id !== "novo"){
-           setIsLoading(true);
+    const handleSalvar = (data: FormValues) => {
+        
+        console.log('Dados do formulário:', data);
 
-           AdmiradoresService.getById(Number(id))
-             .then( (result) => {
-                setIsLoading(false);
-                if( result instanceof Error ) {
+        // incluir
+        if(id === "novo")
+        {
+
+        }
+
+        // alterar
+        if(id !== "novo")
+        {
+
+        }
+    
+
+
+        // Lógica para salvar os dados
+        // AdmiradoresService.save(data).then(result => {
+        //     if (result instanceof Error) {
+        //         alert(result.message);
+        //     } else {
+        //         alert('Registro salvo com sucesso!');
+        //         navigate('/admiradores');
+        //     }
+        // });
+    }
+
+    useEffect(() => {
+        if (id !== "novo") {
+            AdmiradoresService.getById(Number(id))
+            .then((result) => {
+                if (result instanceof Error) {
                     alert(result.message);
                     navigate('/admiradores');
                 } else {
-                   setNome(result.nomeCompleto);                    
-                   console.log(result);
+                    setNome(result.nomeCompleto);
+                    atribuirForm(result);
                 }
-             });
+            });
         }
-    }, [id] );
+        else {
+            limparForm();
+        }
+    }, [id]);
 
-    return(
+    const atribuirForm = (data: IDetalheAdmirador) => {
+        setValue('nomeCompleto', data.nomeCompleto);
+        setValue('email', data.email);
+        setValue('idade', data.idade);
+        setValue('notavelId', data.notavelId);
+    }
+
+    const limparForm= () => {
+        setValue('id',0);
+        setValue('nomeCompleto', '');
+        setValue('email', '');
+        setValue('idade', 0);
+        setValue('notavelId', 0);
+    }
+
+    return (
         <LayOutBaseDePagina 
             titulo={id === "novo" ? 'Novo Admirador' : nome}
             barraDeFerramentas={
                 <FerramentasDeDetalhe 
-                   textoBotaoNovo="Novo"
-                   mostrarBotaoSalvarEFechar
-                   mostrarBotaoApagar = {id !== "novo"}
-                   mostrarBotaoNovo = {id !== "novo"}
-
-                   aoClicarEmSalvar={ () => {} }
-                   aoClicarEmSalvarEFechar={ () => {} }
-                   aoClicarEmApagar={ () => handleDelete(Number(id)) }
-                   aoClicarEmNovo={ () => {navigate('/admiradores/detalhe/novo')} }
-                   aoClicarEmVoltar={ () => {navigate('/admiradores')} }
-
+                    textoBotaoNovo="Novo"
+                    mostrarBotaoSalvarEFechar
+                    mostrarBotaoApagar={id !== "novo"}
+                    mostrarBotaoNovo={id !== "novo"}
+                    aoClicarEmSalvar={handleSubmit(handleSalvar)}
+                    aoClicarEmSalvarEFechar={() => {}}
+                    aoClicarEmApagar={() => handleDelete(Number(id))}
+                    aoClicarEmNovo={() => navigate('/admiradores/detalhe/novo')}
+                    aoClicarEmVoltar={() => navigate('/admiradores')}
                 />
             }
         >
-
-            { isLoading &&
-                (
-                    <LinearProgress variant="indeterminate" />
-                )
-            }
-
-         </LayOutBaseDePagina>
-    )
-
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
+                <Stack spacing={2} width={600} margin={5}>
+                    <TextField
+                        autoComplete='off'
+                        size="small"
+                        label="Nome"
+                        type="text"
+                        variant="outlined"
+                        InputLabelProps={{ shrink: true }}
+                        {...register("nomeCompleto", {
+                            required: "Nome é obrigatório",
+                            minLength: {
+                                value: 5,
+                                message: "O nome deve ter pelo menos 5 letras"
+                            }
+                        })}
+                        error={!!errors.nomeCompleto}
+                        helperText={errors.nomeCompleto?.message}
+                    />
+                    <TextField
+                        autoComplete='off'
+                        size="small"
+                        label="E-Mail"
+                        type="email"
+                        variant="outlined"
+                        InputLabelProps={{ shrink: true }}
+                        {...register("email", {
+                            required: "E-Mail é obrigatório",
+                            pattern: {
+                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                message: "E-Mail inválido"
+                            }
+                        })}
+                        error={!!errors.email}
+                        helperText={errors.email?.message}
+                    />
+                    <Box width={300}>
+                        <TextField
+                            autoComplete='off'
+                            size="small"
+                            label="Idade"
+                            type="number"
+                            variant="outlined"
+                            InputLabelProps={{ shrink: true }}
+                            {...register("idade", {
+                                required: "Idade é obrigatória",
+                                min: {
+                                    value: 10,
+                                    message: "A idade deve ser maior que 10"
+                                }
+                            })}
+                            error={!!errors.idade}
+                            helperText={errors.idade?.message}
+                        />
+                    </Box>
+                </Stack>
+            </form>
+            <DevTool control={control} />
+        </LayOutBaseDePagina>
+    );
 }
+
---
+++ src/shared/services/api/notaveis/NotaveisService.ts
@@ -0,0 +1,94 @@
+import { Enviroment } from "../../../environment";
+import { Api } from "../axios-config";
+
+
+
+export interface IListagemNotavel {
+    id: number;
+    apelido: string;
+    atividade: string;
+    descricao: string;
+}
+
+export interface IDetalheNotavel {
+    id: number;
+    apelido: string;
+    atividade: string;
+    desricao: string;
+}
+
+type TNotavelComTotalCount = {
+    data: IListagemNotavel[];
+    totalCount: number;
+}
+
+const getAll = async (page = 1, filter = ''): Promise<TNotavelComTotalCount | Error> => {
+    try {
+        const urlRelativa = `/notaveis?_page=${page}&_limit=${Enviroment.LIMITE_DE_LINHAS}&nome_like=${filter}`;
+        const { data, headers } = await Api.get(urlRelativa);
+        
+        if (data) {
+            return {
+                data,
+                totalCount: Number(headers['x-total-count'] || Enviroment.LIMITE_DE_LINHAS),
+            };
+        }
+
+        return new Error('Erro ao listar notáveis.');
+    } catch (error) {
+        console.log(error);
+        return new Error((error as { message: string }).message || 'Erro ao listar notáveis.');
+    }
+};
+
+const getById = async (id: number): Promise<IDetalheNotavel| Error> => {
+    try {
+        const { data } = await Api.get(`/notaveis/${id}`);
+        if (data) {
+            return data;
+        }
+        return new Error('Erro ao consultar o notável.');
+    } catch (error) {
+        console.log(error);
+        return new Error((error as { message: string }).message || 'Erro ao consultar o notável.');
+    }
+};
+
+const create = async (notavel: Omit<IDetalheNotavel, 'id'>): Promise<number | Error> => {
+    try {
+        const { data } = await Api.post<IDetalheNotavel>('/notaveis/', notavel);
+        if (data) {
+            return data.id;
+        }
+        return new Error('Erro ao criar o notável.');
+    } catch (error) {
+        console.log(error);
+        return new Error((error as { message: string }).message || 'Erro ao criar o notável.');
+    }
+};
+
+const updateById = async (id: number, notavel: IDetalheNotavel): Promise<void | Error> => {
+    try {
+        await Api.put(`/notaveis/${id}`, notavel);
+    } catch (error) {
+        console.log(error);
+        return new Error((error as { message: string }).message || 'Erro ao alterar o notável.');
+    }
+};
+
+const deleteById = async (id: number): Promise<void | Error> => {
+    try {
+        await Api.delete(`/notaveis/${id}`);
+    } catch (error) {
+        console.log(error);
+        return new Error((error as { message: string }).message || 'Erro ao excluir o notavel.');
+    }
+};
+
+export const AdmiradoresService = {
+    getAll,
+    getById,
+    create,
+    updateById,
+    deleteById,
+};
