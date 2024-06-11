1e77ff12d9e7b96a7d2f7ec9bc371dc48ee1fedc
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-06-11 10:27:17 -0300

    21-Detalhe Admirador - 01

--- src/pages/admiradores/DetalheDeAdmiradores.tsx
+++ src/pages/admiradores/DetalheDeAdmiradores.tsx
@@ -1,6 +1,9 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { LayOutBaseDePagina } from "../../shared/layouts";
 import { FerramentasDeDetalhe } from "../../shared/components";
+import { useEffect, useState } from "react";
+import { AdmiradoresService } from "../../shared/services/api/admiradores/AdmiradoresService";
+import { LinearProgress } from "@mui/material";
 
 
 
@@ -9,9 +12,43 @@ export const DetalheDeAdmiradores: React.FC = () => {
     const { id = 'novo'}  = useParams<'id'>();
     const navigate = useNavigate();
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [nome, setNome] = useState("");
+
+    
+    const handleDelete = (id: number) => {
+        AdmiradoresService.deleteById(id)
+        .then(result => {
+            if (result instanceof Error) {
+              alert(result.message);
+            } else {
+               alert('Registro apagado com sucesso!');
+               navigate('/admiradores');
+            }
+        });
+    }
+
+    useEffect( () => {
+        if(id !== "novo"){
+           setIsLoading(true);
+
+           AdmiradoresService.getById(Number(id))
+             .then( (result) => {
+                setIsLoading(false);
+                if( result instanceof Error ) {
+                    alert(result.message);
+                    navigate('/admiradores');
+                } else {
+                   setNome(result.nomeCompleto);                    
+                   console.log(result);
+                }
+             });
+        }
+    }, [id] );
+
     return(
         <LayOutBaseDePagina 
-            titulo="Detalhe do Admirador"
+            titulo={id === "novo" ? 'Novo Admirador' : nome}
             barraDeFerramentas={
                 <FerramentasDeDetalhe 
                    textoBotaoNovo="Novo"
@@ -21,13 +58,19 @@ export const DetalheDeAdmiradores: React.FC = () => {
 
                    aoClicarEmSalvar={ () => {} }
                    aoClicarEmSalvarEFechar={ () => {} }
-                   aoClicarEmApagar={ () => {} }
+                   aoClicarEmApagar={ () => handleDelete(Number(id)) }
                    aoClicarEmNovo={ () => {navigate('/admiradores/detalhe/novo')} }
                    aoClicarEmVoltar={ () => {navigate('/admiradores')} }
 
                 />
             }
         >
+
+            { isLoading &&
+                (
+                    <LinearProgress variant="indeterminate" />
+                )
+            }
 
          </LayOutBaseDePagina>
     )
