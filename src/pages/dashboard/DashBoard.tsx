3b6818deeeff7e6aa19673ba6deca60affa35cb1
Pedro Bruno R. Lourenço <68498467+pedrobrunolourenco@users.noreply.github.com>
2024-09-18 11:04:44 -0300

    graficos

--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -1,43 +1,45 @@
-import { Box, Card, CardContent, Grid, LinearProgress, Typography } from "@mui/material";
+import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayOutBaseDePagina } from "../../shared/layouts";
 import { useEffect, useState } from "react";
 import { AdmiradoresService } from "../../shared/services/api/admiradores/AdmiradoresService";
-import { Pie } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie, Chart } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, PointElement, LineElement, ArcElement } from 'chart.js';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, LineElement, Title, PointElement);
+
 
 export const DashBoard: React.FC = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [countUf, setTotalUf] = useState<number[]>([]);
     const [labelUf, setLabelUf] = useState<string[]>([]);
     const [countIdade, setTotalIdade] = useState<number[]>([]);
-    const [labelIdade, setLabelIdade] = useState<number[]>([]);
+    const [labelIdade, setLabelIdade] = useState<string[]>([]);
 
     const fetchData = async () => {
         setIsLoading(true);
-
+    
         try {
             const [ufResult, idadeResult] = await Promise.all([
                 AdmiradoresService.getContagemUF(),
                 AdmiradoresService.getContagemIdade()
             ]);
-
+    
             if (ufResult instanceof Error || idadeResult instanceof Error) {
                 return;
             }
-
+    
             if (ufResult.data && Array.isArray(ufResult.data)) {
                 setTotalUf(ufResult.data.map(item => item.count));
                 setLabelUf(ufResult.data.map(item => item.uf));
             } else {
                 console.error("A resposta da UF não contém um array válido:", ufResult);
             }
-
+    
             if (idadeResult.data && Array.isArray(idadeResult.data)) {
                 setTotalIdade(idadeResult.data.map(item => item.count));
-                setLabelIdade(idadeResult.data.map(item => item.idade));
+                setLabelIdade(idadeResult.data.map(item => item.idade.toString()));
             } else {
                 console.error("A resposta da idade não contém um array válido:", idadeResult);
             }
@@ -47,7 +49,7 @@ export const DashBoard: React.FC = () => {
             setIsLoading(false);
         }
     };
-
+    
     useEffect(() => {
         fetchData();
     }, []);
@@ -86,27 +88,27 @@ export const DashBoard: React.FC = () => {
                 label: 'Distribuição Por Idade',
                 data: countIdade,
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 71, 0.5)',   // Tomato
+                    'rgba(0, 255, 255, 0.5)',   // Aqua
+                    'rgba(255, 105, 180, 0.5)',  // Hot Pink
+                    'rgba(255, 165, 0, 0.5)',    // Orange
+                    'rgba(144, 238, 144, 0.5)',  // Light Green
+                    'rgba(75, 0, 130, 0.5)'      // Indigo
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
+                    'rgba(255, 99, 71, 1)', 
+                    'rgba(0, 255, 255, 1)', 
+                    'rgba(255, 105, 180, 1)', 
+                    'rgba(255, 165, 0, 1)', 
+                    'rgba(144, 238, 144, 1)', 
+                    'rgba(75, 0, 130, 1)'
                 ],
                 borderWidth: 1,
             },
         ],
     };
 
-    const options = {
+    const optionsPizza = {
         responsive: true,
         plugins: {
             legend: {
@@ -119,6 +121,75 @@ export const DashBoard: React.FC = () => {
                     },
                 },
             },
+        },
+    };
+
+    const combinedDataUf = {
+        labels: labelUf,
+        datasets: [
+            {
+                type: 'bar' as const,
+                label: 'Bar Dataset',
+                data: countUf,
+                backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+            {
+                type: 'line' as const,
+                label: 'Line Dataset',
+                data: countUf,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                fill: false,
+            },
+        ],
+    };
+
+    const combinedDataIdade = {
+        labels: labelIdade,
+        datasets: [
+            {
+                type: 'bar' as const,
+                label: 'Bar Dataset',
+                data: countIdade,
+                backgroundColor: 'rgba(75, 192, 192, 0.5)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+            {
+                type: 'line' as const,
+                label: 'Line Dataset',
+                data: countIdade,
+                borderColor: 'rgba(255, 99, 132, 1)',
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                fill: false,
+            },
+        ],
+    };
+
+
+    const optionsBar = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top' as const,
+          },
+          tooltip: {
+            callbacks: {
+                label: function (tooltipItem: any) {
+                    return `${tooltipItem.label}: ${tooltipItem.raw}`;
+                },
+            },
+          },
+        },
+        scales: {
+          x: {
+            stacked: false,
+          },
+          y: {
+            stacked: false,
+          },
         },
     };
 
@@ -135,8 +206,8 @@ export const DashBoard: React.FC = () => {
                                 <Typography variant="h5" align="center">
                                     Admiradores Distribuídos por UF (TOP 6)
                                 </Typography>
-                                <Box padding={2} display="flex" justifyContent="center" alignItems="center">
-                                    {isLoading ? <Typography>Carregando...</Typography> : <Pie data={dataUf} options={options} />}
+                                <Box padding={2} display="flex" justifyContent="center" alignItems="center"  style={{ height: 250 }}>
+                                    {isLoading ? <Typography>Carregando...</Typography> : <Pie data={dataUf} options={optionsPizza} />}
                                 </Box>
                             </CardContent>
                         </Card>
@@ -147,14 +218,32 @@ export const DashBoard: React.FC = () => {
                                 <Typography variant="h5" align="center">
                                     Admiradores Distribuídos por Idade (TOP 6)
                                 </Typography>
-                                <Box padding={2} display="flex" justifyContent="center" alignItems="center">
-                                    {isLoading ? <Typography>Carregando...</Typography> : <Pie data={dataIdade} options={options} />}
+                                <Box padding={2} display="flex" justifyContent="center" alignItems="center"  style={{ height: 250 }}>
+                                    {isLoading ? <Typography>Carregando...</Typography> : <Pie data={dataIdade} options={optionsPizza} />}
                                 </Box>
                             </CardContent>
                         </Card>
                     </Grid>
-                </Grid>
+                    <Grid item xs={12} sm={6} md={6} lg={6}>
+                        <Card>
+                            <CardContent>
+                                <Box padding={2} display="flex" justifyContent="center" alignItems="center"  style={{ height: 250 }}>
+                                   <Chart type="bar" data={combinedDataUf} options={optionsBar} />
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6} lg={6}>
+                        <Card>
+                            <CardContent>
+                                <Box padding={2} display="flex" justifyContent="center" alignItems="center"  style={{ height: 250 }}>
+                                   <Chart type="bar" data={combinedDataIdade} options={optionsBar} />
+                                </Box>
+                            </CardContent>
+                        </Card>
+                    </Grid>
 
+                </Grid>
             </Box>
         </LayOutBaseDePagina>
     );
